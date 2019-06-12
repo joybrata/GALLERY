ede7cbb6bccbc1e2eeeb6898ee05f442d227e2ab
joybrata <joybratapal1995@gmail.com>
2019-06-13 04:55:59 +0530

    nav bar selected item is done

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  homeSelected: boolean = true;
+  artistsSelected: boolean = false;
+  aboutSelected: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectedFunction(x: number) {
+    this.homeSelected = false;
+    this.artistsSelected = false;
+    this.aboutSelected = false;
+    console.log('clicked');
+    console.log(x);
+    switch (x) {
+      case 1: {
+        this.homeSelected = true;
+        break;
+      }
+      case 2: {
+        this.artistsSelected = true;
+        break;
+      }
+      case 3: {
+        this.aboutSelected = true;
+        break;
+      }
+      default: {
+        console.log('Default Case selected in switch case');
+      }
+    }
+  }
 }
